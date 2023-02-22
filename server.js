0bc5122375f036da5f004e0031c5949c72383d51
JacobKnaack <jacobknck@gmail.com>
2023-02-22 14:52:34 -0800

    adds initial code

---
+++ server.js
@@ -0,0 +1,28 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const app = express();
+
+const CLIENT_ID = process.env.UNSPLASH_CLIENT_ID;
+const PORT = process.env.PORT;
+
+app.use(cors());
+
+app.get('/photos', async (request, response) => {
+
+  // creates second request to unsplash
+  let proxy = {
+    url: `https://api.unsplash.com/search/photos?page=1&query=${request.query.searchQuery}&client_id=${CLIENT_ID}`, // what lives here??? Query String parameters
+    method: 'GET'
+  };
+  let proxyRes = await axios(proxy);
+  response.status(200).send(proxyRes.data);
+});
+
+
+app.listen(PORT, ()=> {
+  console.log('App is running');
+});
